2d064197fe2e3eccbdef634b2571f3d6a400a41f
shyye <emma.art.dev@gmail.com>
2024-09-26 20:24:28 +0200

    Fix Extension part 1

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const cardsUL = document.querySelector(".cards")
 function renderCards() {
 
     data.forEach((pokemon) => {
-        
+
         const li = document.createElement("li")
         li.classList.add("card")
         li.innerHTML = `
@@ -31,8 +31,20 @@ function renderCards() {
                 <li>${stats.stat.name.toUpperCase()}: ${stats.base_stat}</li>
             `
         })
-
         li.append(innerUL)
+
+        // games each pokemon appeared in
+        const p = document.createElement("p")
+        p.innerHTML = `
+            Appears in:            
+        `
+
+        // Assumes that the games is the name, even if the indices are the same for some of them
+        pokemon.game_indices.forEach((game) => {
+            p.innerHTML += game.version.name + ", "
+        })
+        li.append(p)
+
         cardsUL.append(li)
     })
 }
